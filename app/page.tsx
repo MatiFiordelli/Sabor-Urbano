1439b74ffa8fac0485dafc972d06c85bfbc7678e
Matias Fiordelli <>
2024-11-13 22:18:38 -0300

    minor correction

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import CardsGroupBurgers from "@/components/CardsGroupBurgers";
 import Marquee from "@/components/Marquee";
 import { useEffect, useRef } from "react";
-import Waves from "@/components/Canvas/Waves";
+//import Waves from "@/components/Canvas/Waves";
 import TopPresentation from "@/components/TopPresentation";
 
 export default function Home() {
